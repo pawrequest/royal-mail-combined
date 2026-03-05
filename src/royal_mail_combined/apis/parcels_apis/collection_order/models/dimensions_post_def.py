2ff36fef076224d568e476355671cea09fdab28d
Paw <paw@pawrequest.com>
2026-03-05 18:56:54 +0000

    rm to_str all models

--- src/royal_mail_combined/apis/parcels_apis/collection_order/models/dimensions_post_def.py
+++ src/royal_mail_combined/apis/parcels_apis/collection_order/models/dimensions_post_def.py
@@ -38,10 +38,6 @@ class DimensionsPostDef(BaseModel):
     )
 
 
-    def to_str(self) -> str:
-        """Returns the string representation of the model using alias"""
-        return pprint.pformat(self.model_dump(by_alias=True))
-
     def to_json(self) -> str:
         """Returns the JSON representation of the model using alias"""
         # TODO: pydantic v2: use .model_dump_json(by_alias=True, exclude_unset=True) instead
