fbd5359dce6180459c20e50ae77bf9d846a74e8a
Paw <paw@pawrequest.com>
2026-05-06 11:34:39 +0100

    oneshots get colelction

--- src/royal_mail_combined/one_shots.py
+++ src/royal_mail_combined/one_shots.py
@@ -9,18 +9,20 @@ def client():
 
 
 def delete_all_orders():
+    print('NOPE')
+    return
     fxt_client = client()
     orders_after = fxt_client.fetch_orders().orders
     sure = input(
         f'This will delete the following orders, are you sure?\n{[_.order_identifier for _ in orders_after]}\n\n (y/n)'
     )
-    if not sure.lower() == 'y':
-        print('Aborting')
-        return
-    for o in orders_after.orders:
-        res = fxt_client.cancel_outbound_shipment(order_identifiers=str(o.order_identifier))
-        assert o.order_identifier in res.order_idents(), 'WARNING, FAILED TO DELETE TEST ORDERS!!'
-        print('Deleted Order')
+    if sure.lower() == 'y':
+        for o in orders_after.orders:
+            res = fxt_client.cancel_outbound_shipment(order_identifiers=str(o.order_identifier))
+            assert o.order_identifier in res.order_idents(), 'WARNING, FAILED TO DELETE TEST ORDERS!!'
+            print('Deleted Order')
+    print('Aborting')
+    return
 
 
 def cancel_collection(collection_id):
@@ -29,8 +31,15 @@ def cancel_collection(collection_id):
     print(res)
 
 
+def get_collection(collection_id):
+    fxt_client = client()
+    res = fxt_client.parcel_api.collection_orders_api.order_get(collection_id)
+    print(res)
+    ...
+
+
 if __name__ == '__main__':
     # delete_all_orders()
-    c1 = 'CC-W307-135088399'
-    c2 = 'CC-W307-135088435'
-    cancel_collection(collection_id=c2)
+    c1 = 'CC-W307-137095301'
+    # get_collection(collection_id=c1)
+    # cancel_collection(collection_id=c1)
